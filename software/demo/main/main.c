2dd398b8a96b20480d9c8bb16844b3c321403fa6
bendelathouwer <bendelathouwer@gmail.com>
2025-05-21 17:36:39 +0200

    update

---
+++ software/demo/main/main.c
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include "driver/sdmmc_host.h"
+#include "sd_pwr_ctrl_by_on_chip_ldo.h"
+
+void app_main(void)
+{
+   
+}
